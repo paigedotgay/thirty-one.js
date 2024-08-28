b23760f2c333152f303f08894291eb4adcb29a12
paige <paige@paige.gay>
2024-08-27 22:31:22 -0600

    added decLife as todo

--- gamestate.js
+++ gamestate.js
@@ -16,3 +16,25 @@ function newGamestate() {
         "players": [],
     }
 }
+
+/**
+ * Decreases a player's life by 1
+ * @param {*} gamestate current gamestate
+ * @param {*} playerIndex the index of the player to lose life
+ */
+function decLife(gamestate, playerIndex) {
+    /**
+     * TODO:
+     * ok so tbh I'm not sure how I'm gonna do this part.
+     * I'm trying to do this in a 100% functional way
+     * but js doesn't seem to like that very much.
+     * 
+     * might do some weird destructuring and restructuring thing
+     * like let { players, ...rest } = gamestate;
+     * players[playerIndex].life--;
+     * return {players, ...rest};
+     * 
+     * but that middle line feels awfully side-effect-y.
+     * need to think more.
+     */
+}
